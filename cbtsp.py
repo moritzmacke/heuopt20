9ad64c4a29b6542bb89b6d9dff7b30c01ad8c8e3
Michael Maurer <maurerm3141@gmail.com>
2020-11-23 11:25:35 +0100

    Add delta evaluation for 3-opt and short block move

--- cbtsp.py
+++ cbtsp.py
@@ -1,4 +1,3 @@
- 
 import random
 import numpy as np
 import math
@@ -13,8 +12,9 @@ from pymhlib.gvns import GVNS
 from pymhlib.scheduler import Method
 from pymhlib.solution import Solution
 
-#from pymhlib.settings import get_settings_parser
+# from pymhlib.settings import get_settings_parser
 from hamcycle import HamCycle
+
 
 class CBTSPInstance:
     """
@@ -38,22 +38,22 @@ class CBTSPInstance:
             for line in lines[1:]:
                 s = line.split()
                 n1, n2, w = int(s[0]), int(s[1]), int(s[2])
-                edges.append((n1,n2,w))
-               
-        ws = sorted([ e[2] for e in edges ])
+                edges.append((n1, n2, w))
+
+        ws = sorted([e[2] for e in edges])
         minw = sum(ws[0:n])
         maxw = sum(ws[-n:])
-        #M = maxw - min(minw,0)
-        M = max(abs(minw),abs(maxw)) - sum(ws[0:n-1]) + 1
+        # M = maxw - min(minw,0)
+        M = max(abs(minw), abs(maxw)) - sum(ws[0:n - 1]) + 1
 
         print(M)
-            
+
         # adjacency matrix
-        weights = np.full((n,n),M)
+        weights = np.full((n, n), M)
         for (n1, n2, w) in edges:
-#            print(n1,n2,w)
-            weights[n1][n2] = weights[n2][n1] = w #
-            
+            #            print(n1,n2,w)
+            weights[n1][n2] = weights[n2][n1] = w  #
+
         self.weights = weights
         self.n = n
         self.valid_threshold = maxw
@@ -63,8 +63,8 @@ class CBTSPInstance:
     def __repr__(self):
         """Write out the instance data."""
         return f"n={self.n},\nweights={self.weights!r}\n"
-    
-    
+
+
 class CBTSPSolution(PermutationSolution):
     """Solution to a TSP instance.
 
@@ -89,24 +89,24 @@ class CBTSPSolution(PermutationSolution):
         for i in range(self.inst.n - 1):
             w += self.inst.weights[self.x[i]][self.x[i + 1]]
         w += self.inst.weights[self.x[-1]][self.x[0]]
-        return w #abs(w)
+        return w  # abs(w)
 
     def check(self):
         """Check if valid solution.
 
         :raises ValueError: if problem detected.
         """
-        
-        #?
+
+        # ?
         if self.obj() > self.inst.valid_threshold:
             invalid_edges = []
             for i in range(self.inst.n):
-                j = (i+1) % self.inst.n
-                p,q = self.x[i], self.x[j]
-                if self.inst.weights[p,q] == self.inst.bigM:
-                    invalid_edges.append((p,q))
+                j = (i + 1) % self.inst.n
+                p, q = self.x[i], self.x[j]
+                if self.inst.weights[p, q] == self.inst.bigM:
+                    invalid_edges.append((p, q))
             raise ValueError("{} invalid edges used in solution: {}".format(len(invalid_edges), invalid_edges))
-        
+
         if len(self.x) != self.inst.n:
             raise ValueError("Invalid length of solution")
         super().check()
@@ -118,7 +118,7 @@ class CBTSPSolution(PermutationSolution):
 
         """
         if par == 0:
-            self.initialize(par) # random order of nodes
+            self.initialize(par)  # random order of nodes
         elif par == 1:
             self.nn_const_heuristic_single(0)
         elif par == 2:
@@ -141,56 +141,56 @@ class CBTSPSolution(PermutationSolution):
         Does not work great the way it is, because a selected next point can
         force you to choose an invalid edge to insert in current tour...
         """
-        
+
         w = self.inst.weights
         n = self.inst.n
-        
-        #start with shortest edge, is not necessesarily best though?
-        edges = sorted(self.inst.edges, key = lambda e: abs(e[2]))
-#        print(edges)
+
+        # start with shortest edge, is not necessesarily best though?
+        edges = sorted(self.inst.edges, key=lambda e: abs(e[2]))
+        #        print(edges)
         p1 = edges[0][0]
         p2 = edges[0][1]
-        newx = [p1,p2]
+        newx = [p1, p2]
         ps = [p for p in range(n) if p not in newx]
         random.shuffle(ps)
-        
-#        print(ps)
-        
-        cur_obj = 2*w[p1][p2]
- #       print(cur_obj)
-        
-        for k in range(2,n):
+
+        #        print(ps)
+
+        cur_obj = 2 * w[p1][p2]
+        #       print(cur_obj)
+
+        for k in range(2, n):
             if select_random:
                 p = ps.pop()
-            else: #select farthest point for next insert
-                #does not seem to work well though and is slow...
+            else:  # select farthest point for next insert
+                # does not seem to work well though and is slow...
                 farthest_w = 0
                 farthest_p = None
                 for p in ps:
-                    best_w = min([ abs(w[p][q]) for q in newx])
+                    best_w = min([abs(w[p][q]) for q in newx])
                     if best_w > farthest_w:
                         farthest_w = best_w
                         farthest_p = p
-#                    print("{} closest dist: {}".format(p, best_w))
+                #                    print("{} closest dist: {}".format(p, best_w))
                 p = farthest_p
                 ps.remove(p)
             best_j = 0
             p1 = newx[0]
             p2 = newx[1]
             best_delta = w[p1][p] + w[p][p2] - w[p1][p2]
-#            print("obj insert {} between {} and {}: {}".format(p, p1, p2, best_delta+cur_obj))
-            for j in range(1,k):
+            #            print("obj insert {} between {} and {}: {}".format(p, p1, p2, best_delta+cur_obj))
+            for j in range(1, k):
                 p1 = newx[j]
-                p2 = newx[(j+1)%k]
+                p2 = newx[(j + 1) % k]
                 delta = w[p1][p] + w[p][p2] - w[p1][p2]
-#                print("obj insert {} between {} and {}: {}".format(p, p1,p2,delta+cur_obj))
-                if abs(cur_obj+delta) < abs(cur_obj+best_delta):
+                #                print("obj insert {} between {} and {}: {}".format(p, p1,p2,delta+cur_obj))
+                if abs(cur_obj + delta) < abs(cur_obj + best_delta):
                     best_j = j
                     best_delta = delta
-            newx.insert(best_j+1, p)
+            newx.insert(best_j + 1, p)
             cur_obj += best_delta
-#            print(newx)
-        
+        #            print(newx)
+
         self.x[:] = newx
         self.invalidate()
 
@@ -198,33 +198,33 @@ class CBTSPSolution(PermutationSolution):
         """Nearest neighbor-like heuristic, minimze cost of adding new point to tour.
         For single starting point, could repeat for every starting point
         """
-        
+
         w = self.inst.weights
         n = self.inst.n
-        
+
         x = [starting_point]
         ps = [q for q in range(n) if q != starting_point]
-        
-#        print(ps)
-        
-        cur_edge_sum = 0 #doesn't count return edge to close tour
- #       print(cur_obj)
-        
-        for k in range(1,n):
-            p0, p = x[0], x[k-1]
+
+        #        print(ps)
+
+        cur_edge_sum = 0  # doesn't count return edge to close tour
+        #       print(cur_obj)
+
+        for k in range(1, n):
+            p0, p = x[0], x[k - 1]
             best_q = ps[0]
-            best_new_obj = cur_edge_sum + w[p][best_q] + w[best_q][p0] 
+            best_new_obj = cur_edge_sum + w[p][best_q] + w[best_q][p0]
             for q in ps:
                 new_obj = cur_edge_sum + w[p][q] + w[q][p0]
-#                print("add {} to tour: {}".format(q, new_obj))
+                #                print("add {} to tour: {}".format(q, new_obj))
                 if abs(new_obj) < abs(best_new_obj):
                     best_q = q
                     best_new_obj = new_obj
             x += [best_q]
             cur_edge_sum += w[p][best_q]
             ps.remove(best_q)
-#            print(x, cur_edge_sum + w[best_q][p0], ps)
-        
+        #            print(x, cur_edge_sum + w[best_q][p0], ps)
+
         self.x[:] = x
         self.invalidate()
 
@@ -238,11 +238,11 @@ class CBTSPSolution(PermutationSolution):
         edges = sorted(self.inst.edges, key=lambda e: e[2])
         setof = [i for i in range(n)]
         seledges = [[] for i in range(n)]
-#        print(edges, setof, seledges)
-        
+        #        print(edges, setof, seledges)
+
         k = 0
         cur_obj = 0
-        
+
         while edges:
             cand_edges = []
             for e in edges:
@@ -252,7 +252,7 @@ class CBTSPSolution(PermutationSolution):
                 if setof[p] == setof[q] and k < n:
                     continue
                 cand_edges.append(e)
-#            print("filtered edges", cand_edges)
+            #            print("filtered edges", cand_edges)
             if not cand_edges:
                 break
             best_e = random.choice(cand_edges)
@@ -263,25 +263,24 @@ class CBTSPSolution(PermutationSolution):
                     if abs(new_obj) < abs(best_obj):
                         best_e = e
                         best_obj = new_obj
-#            print("best edge", best_e)
-            p,q = best_e[0], best_e[1]
+            #            print("best edge", best_e)
+            p, q = best_e[0], best_e[1]
             seledges[p].append(q)
             seledges[q].append(p)
-            sq,sp = setof[q], setof[p]
-            spq = min(sq,sp)
+            sq, sp = setof[q], setof[p]
+            spq = min(sq, sp)
             for i in range(n):
                 if setof[i] == sq or setof[i] == sp:
                     setof[i] = spq
-#            print(["{}:{}".format(i,e) for (i,e) in enumerate(seledges)])
-#            print(setof)
+            #            print(["{}:{}".format(i,e) for (i,e) in enumerate(seledges)])
+            #            print(setof)
             edges = cand_edges
             cur_obj = best_obj
             k = k + 1
-                
-        
-#        print(k)
+
+        #        print(k)
         # construct tour from fragments, will have to add invalid edges
-        
+
         # this whole procedure is pretty ugly and probably not correct...
         x = []
         visited = [False for i in range(n)]
@@ -297,22 +296,22 @@ class CBTSPSolution(PermutationSolution):
                 p = q
                 while len(seledges[p]) > 1 and not visited[p]:
                     visited[p] = True
-                    q1,q2 = seledges[p][0], seledges[p][1]
+                    q1, q2 = seledges[p][0], seledges[p][1]
                     q = q1 if not visited[q1] else q2
                     if visited[q]:
                         break
                     ps.append(q)
                     p = q
                 visited[p] = True
-                q = seledges[p][0] #must be len(seledges[p] == 1 here, unless we have a full cycle?
+                q = seledges[p][0]  # must be len(seledges[p] == 1 here, unless we have a full cycle?
                 if not visited[q]:
                     ps.append(q)
                     visited[q] = True
-                
+
                 # search in other direction
-                p = i  
+                p = i
                 while len(seledges[p]) > 1:
-                    q1,q2 = seledges[p][0], seledges[p][1]
+                    q1, q2 = seledges[p][0], seledges[p][1]
                     q = q1 if not visited[q1] else q2
                     if visited[q]:
                         break
@@ -323,24 +322,21 @@ class CBTSPSolution(PermutationSolution):
                 if not visited[q]:
                     ps = [q] + ps
                     visited[q] = True
-                    
-#            print(ps)
+
+            #            print(ps)
             x += ps
 
-        
-#        print(x, [i for i in x if x.count(i) > 1])
+        #        print(x, [i for i in x if x.count(i) > 1])
         self.x[:] = x
         self.invalidate()
 
- 
-
     def is_better(self, other: "Solution") -> bool:
         """Return True if the current solution is better in terms of the objective function than the other."""
-        return abs(self.obj()) < abs(other.obj()) 
+        return abs(self.obj()) < abs(other.obj())
 
     def is_worse(self, other: "Solution") -> bool:
         """Return True if the current solution is worse in terms of the objective function than the other."""
-        return abs(self.obj()) > abs(other.obj()) 
+        return abs(self.obj()) > abs(other.obj())
 
     @classmethod
     def is_better_obj(cls, obj1: TObj, obj2: TObj) -> bool:
@@ -352,15 +348,7 @@ class CBTSPSolution(PermutationSolution):
         """Return True if obj1 is a worse objective value than obj2."""
         return abs(obj1) > abs(obj2)
 
-
-
-
-
-
-
-
-
-#
+    #
     def shaking(self, par, result):
         """Scheduler method that performs shaking by 'par'-times swapping a pair of randomly chosen cities."""
         for _ in range(par):
@@ -372,7 +360,7 @@ class CBTSPSolution(PermutationSolution):
 
     def local_improve(self, _par, _result):
         self.own_two_opt_neighborhood_search(True)
-        
+
     #
     def own_two_opt_neighborhood_search(self, best_improvement) -> bool:
         """Systematic search of the 2-opt neighborhood, i.e., consider all inversions of subsequences.
@@ -385,7 +373,7 @@ class CBTSPSolution(PermutationSolution):
 
         :return: True if an improved solution has been found
         """
-               
+
         n = self.inst.n
         best_delta = 0
         best_p1 = None
@@ -423,7 +411,7 @@ class CBTSPSolution(PermutationSolution):
         assert (p1 < p2)
         n = len(self.x)
         if p1 == 0 and p2 == n - 1:
-           # reversing the whole solution has no effect
+            # reversing the whole solution has no effect
             return 0
         prev = (p1 - 1) % n
         nxt = (p2 + 1) % n
@@ -433,6 +421,55 @@ class CBTSPSolution(PermutationSolution):
         x_next = self.x[nxt]
         d = self.inst.weights
         delta = d[x_prev][x_p2] + d[x_p1][x_next] - d[x_prev][x_p1] - d[x_p2][x_next]
+
+        return delta
+
+    def three_opt_move_delta_eval(self, p1: int, p2: int, p3: int) -> int:
+        """ Performs delta evaluation for moving the subsequence from p2 to p3 before p1 in self.x.
+
+        The function returns the difference in the objective function if the move would be performed,
+        the solution, however, is not changed.
+        """
+        assert (p1 < p2)
+        assert (p2 < p3)
+        n = len(self.x)
+
+        x_p1_pred = self.x[(p1 - 1) % n]
+        x_p1 = self.x[p1]
+        x_p2_pred = self.x[(p2 - 1) % n]
+        x_p2 = self.x[p2]
+        x_p3 = self.x[p3]
+        x_p3_succ = self.x[(p3 + 1) % n]
+        d = self.inst_weights
+
+        # Added edges: predecessor of p1 to p2, p3 to p1, predecessor of p2 to successor of p3
+        # Lost edges: predecessor of p1 to p1, predecessor of p2 to p2, p3 to successor of p3
+        delta = d[x_p1_pred][x_p2] + d[x_p3][x_p1] + d[x_p2_pred][x_p3_succ] \
+            - d[x_p1_pred][x_p1] - d[x_p2_pred][x_p2] - d[x_p3][x_p3_succ]
+
+        return delta
+
+    def short_block_delta_eval(self, p1: int, p2: int) -> int:
+        """ Performs delta evaluation for moving the subsequence of length 3 starting at p2 before p1 in self.x.
+
+        The function returns the difference in the objective function if the move would be performed,
+        the solution, however, is not changed.
+        """
+        assert (p1 < p2)
+        n = len(self.x)
+
+        x_p1_pred = self.x[(p1 - 1) % n]
+        x_p1 = self.x[p1]
+        x_p2_pred = self.x[(p2 - 1) % n]
+        x_p2 = self.x[p2]
+        x_subseq_end = self.x[(p2 + 2) % n]
+        x_subseq_end_succ = self.x[(p2 + 3) % n]
+        d = self.inst_weights
+
+        # Added edges: predecessor of p1 to p2, subseq end to p1, predecessor of p2 to successor of subseq end
+        # Lost edges: predecessor of p1 to p1, predecessor of p2 to p2, subseq end to successor of subseq end
+        delta = d[x_p1_pred][x_p2] + d[x_subseq_end][x_p1] + d[x_p2_pred][x_subseq_end_succ] \
+            - d[x_p1_pred][x_p1] - d[x_p2_pred][x_p2] - d[x_subseq_end][x_subseq_end_succ]
 
         return delta
 
@@ -450,7 +487,7 @@ class CBTSPSolution(PermutationSolution):
 
         Works the same way as apply_two_opt_move from the base class, so no value update or invalidation is done.
         """
-        self.x = self.x[:p1] + self.x[p2:(p3+1)] + self.x[p1:p2] + self.x[(p3+1):]
+        self.x = self.x[:p1] + self.x[p2:(p3 + 1)] + self.x[p1:p2] + self.x[(p3 + 1):]
 
     def apply_short_block_move(self, p1: int, p2: int):
         """The subsequence of length 3 starting at p2 is moved before p1 in self.x.
